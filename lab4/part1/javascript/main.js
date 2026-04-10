517fd5f2b42c59f217425f2593c2cf15674dd7c0
JJJaxson <Jaxson.jeanjacques@dcmail.ca>
2026-04-10 16:47:50 -0400

    commit 5

--- lab4/part1/javascript/main.js
+++ lab4/part1/javascript/main.js
@@ -22,7 +22,7 @@ function returnRandomStoryString() {
     const randomEvent = randomValueFromArray(events);
 
     let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, 
-    they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — 
+    they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised - 
     ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
     return storyText;
 }
@@ -41,11 +41,13 @@ function generateStory() {
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300 / 14);
-    const temperature = Math.round((94 - 32) * (5 / 9));
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature = `${Math.round((94 - 32) * (5 / 9))} Celsius`;
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);  
   }
 
   // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
 }
